2d38daa80b7b7b675de8ca44ad16bea11958f1cc
dylanmcca <dylanmcca@gmail.com>
2025-11-27 20:01:53 +0000

    last changes made during de-bugging exercise

--- blog/models.py
+++ blog/models.py
@@ -8,28 +8,31 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     content = models.TextField()
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
-        
 
-class Meta:
+    class Meta:
         ordering = ["-created_on"]
 
-
-def __str__(self):
-    return f"The title of this post is {self.title}"    
-
-    # I added excerpt field above as part of the challenge
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ["created_on"]
 
+    def __str__(self):
+        return f"{self.body} by {self.author}"
